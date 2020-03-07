4f3f3a1cbcf251cbb5a156fad85bf2fa94dfae31
mao chao <6233843@gmail.com>
2020-03-07 14:08:48 +0800

    refactor

--- src/login_middleware.js
+++ src/login_middleware.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
     if (req.headers.authorization === 'Bearer 89e38e0ca1c5857c5f848d49fcb825f72926635a') {
       next()
     } else {
-      res.status(400).json({ message: 'Need Authentication' })
+      res.status(401).json({ message: 'Need Authentication' })
     }
   }
 }
--- src/server.js
+++ src/server.js
@@ -9,14 +9,16 @@ server.use(middlewares)
 server.use(jsonServer.bodyParser)
 server.use(loginMiddlewares)
 
-
 server.post('/login', (req, res) => {
   if (req.body.username == 'admin' && req.body.password=="password")
     res.jsonp({ "token": "89e38e0ca1c5857c5f848d49fcb825f72926635a"})
+  else{
+    res.status(400).jsonp({ "message": "Wrong username/password" })
+  }
 })
 
 server.use(router)
 
-server.listen(3000, () => {
+server.listen(12306, () => {
   console.log('JSON Server is running')
 })
